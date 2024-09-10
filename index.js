00ae0e13613e41f4b285a1b5cfb8d121eb9af587
Zeke Sikelianos <zeke@sikelianos.com>
2024-09-10 10:02:33 -0700

    explain it in the output

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
 console.log("Look ma, no registry!");
+
+console.log("This is a package you installed directly from GitHub!");
+
+console.log("To see how this works, check out https://github.com/zeke/look-ma-no-registry")
